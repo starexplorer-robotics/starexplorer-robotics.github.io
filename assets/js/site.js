86b489ddff31dbd78fe4a6b8aa084f3c3ea65d14
Bichy <bichyzhang@gmail.com>
2026-03-07 18:38:50 +0800

    [Init] Website V1.0 Online
    Signed-off-by: Bichy <bichyzhang@gmail.com>

---
+++ assets/js/site.js
@@ -0,0 +1,23 @@
+const toggle = document.querySelector("[data-nav-toggle]");
+const nav = document.querySelector("[data-site-nav]");
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = nav.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", String(isOpen));
+  });
+
+  nav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("is-open");
+      toggle.setAttribute("aria-expanded", "false");
+    });
+  });
+}
+
+const syncHeaderState = () => {
+  document.body.classList.toggle("scrolled", window.scrollY > 12);
+};
+
+syncHeaderState();
+window.addEventListener("scroll", syncHeaderState, { passive: true });
